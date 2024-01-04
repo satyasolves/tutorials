a0dc2cce68f17a06805f5a24686ed941a03e8e34
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2024-01-03 23:46:28 -0600

    Test Commit6

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         //testing commits
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.yellow,
       ),
       home: const MyHomePage(title: 'Flutter Commit Testing'),
     );
@@ -64,12 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
