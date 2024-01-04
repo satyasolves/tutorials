aa61fb6892cfab315c9b7789f5b01fefb459a1c6
satyasolves <satyasolvesolutions@gmail>
2024-01-04 09:23:07 -0600

    Test Commit6

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
+          // test commit for second git hub account
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
