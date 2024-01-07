a551c0939b33ce5f9f87729a34f42ab685be509d
satyasolves <satyasolvesolutions@gmail>
2024-01-06 19:51:17 -0600

    changed the title now

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter  Demo 9',
+      title: 'Flutter  Demo Test',
       theme: ThemeData(
 
         primarySwatch: Colors.green,
