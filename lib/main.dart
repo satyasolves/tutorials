f389429420e12d7f7688080f95b3be0e5bb5019b
satyasolves <satyasolvesolutions@gmail.com>
2024-01-09 23:57:22 -0600

    basic ui code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
-// test flutter code
-void main() => runApp(MaterialApp(
-  home: Text('Hello World!'),
-));
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Basic UI',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(
+            'Hello, Flutter!',
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
